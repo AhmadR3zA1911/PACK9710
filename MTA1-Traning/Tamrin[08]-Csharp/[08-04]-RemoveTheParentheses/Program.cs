e4cf638fc3825c74d406c7e58bda225f9452c565
AhmadR3zA1911 <46918547+AhmadR3zA1911@users.noreply.github.com>
2019-02-25 00:51:08 +0330

    [08]

--- MTA1-Traning/Tamrin[08]-Csharp/[08-04]-RemoveTheParentheses/Program.cs
+++ MTA1-Traning/Tamrin[08]-Csharp/[08-04]-RemoveTheParentheses/Program.cs
@@ -8,45 +8,40 @@ namespace _08_04__RemoveTheParentheses
 {
     class Program
     {
-        static string reverse_remove_parentheses(string str)
-        {
-            int CheckPar = str.LastIndexOf('(');
-            if (CheckPar == -1)
-            {
-                return str;
-            }
-            else
-            {
-                int rid = str.IndexOf(')', CheckPar);
-
-                return reverse_remove_parentheses(
-                      str.Substring(0, CheckPar)
-                    + new string(str.Substring(CheckPar + 1, rid - CheckPar - 1).Reverse().ToArray())
-                    + str.Substring(rid + 1)
-                );
-            }
-        }
-
+ 
 
 
         static void Main(string[] args)
         {
-            Console.WriteLine(reverse_remove_parentheses("p(rq)st"));
-            Console.WriteLine(reverse_remove_parentheses("(p(rq)st)"));
-            Console.WriteLine(reverse_remove_parentheses("ab(cd(ef)gh)ij"));
-            //string Text = "" ;
-            //Console.Write("Please Enter String For Remove ->():");
-            //Text = Console.ReadLine();
-            //string NewText = "";
-            //for (int i = 0; i < Text.Length; i++)
-            //{
-            //    if (Text[i]!='(' && Text[i] !=')')
 
-            //    {
-            //        NewText += Text[i];
-            //    }
-            //}
-            //Console.WriteLine($"New Text Without ():{NewText}");
+            string Text = "";
+            string Temp = "";
+            Console.Write("Please Enter String For Remove ->():");
+            Text = Console.ReadLine();
+            string NewText = "";
+            for (int i = 0; i < Text.Length; i++)
+            {
+                if (Text[i] != '(' && Text[i] != ')')
+
+                {
+                    NewText += Text[i];
+                }
+                else if (Text[i] == '(')
+                {
+                    while (Text[i] != ')')
+                    {
+                        if (Text[i] != '(' && Text[i] != ')')
+
+                            Temp += Text[i++];
+                        else
+                            i++;
+                    }
+                    NewText += new string(Temp.Reverse().ToArray()); //reverse
+            
+                    Temp = "";
+                }
+            }
+            Console.WriteLine($"New Text Without ():{NewText}");
 
 
             Console.ReadKey();
---
+++ MTA1-Traning/Tamrin[08]-Csharp/[08-05]-DuplicateNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_05__DuplicateNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Num = new int[] { 1, 2,5,6,9 ,8,2,5,1 };
+            int count = 0;
+          
+
+                for (int i = 0; i < Num.Length; i++)
+                {
+                
+                    for (int j = i+1; j < Num.Length; j++)
+                    {
+                        if (Num[i] == Num[j])
+                            count++;
+
+                        if (count >= 1)
+                        {
+                            Console.WriteLine($"Total number of duplicate elements found in the array is :{Num[i]}");
+                            count = 0;
+                            break;
+                        }
+                        
+
+                    }
+                
+
+
+
+                }
+            
+            Console.ReadKey();
+        }
+    }
+}
