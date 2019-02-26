ec566e45425c601402ab332baa9c490658f7c39b
AhmadR3zA1911 <46918547+AhmadR3zA1911@users.noreply.github.com>
2019-02-27 00:52:43 +0330

    [08]

---
+++ MTA1-Traning/Tamrin[08]-Csharp/[08-07]-DeleteElement/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_07__DeleteElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str;
+            string[] strnew;
+            int indexCount = 0;
+            Console.WriteLine("Enter The Number Of Element To be Stored in The Array");
+            indexCount = Int32.Parse(Console.ReadLine());
+            str = new string[indexCount];
+            strnew = new string[indexCount];
+            for (int i = 0; i < indexCount; i++)
+            {
+                Console.Write($"Enter Element {i + 1}: ");
+                str[i] = Console.ReadLine();
+            }
+            Console.Write("Input the position where to delete:");
+            string rem = Console.ReadLine();
+            for (int i = 0; i < indexCount; i++)
+            {
+                if (str[i] != rem)
+                    strnew[i] = str[i];
+            }
+            Console.Write("The new list is:");
+            for (int i = 0; i < strnew.Length; i++)
+            {
+                Console.Write(strnew[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
