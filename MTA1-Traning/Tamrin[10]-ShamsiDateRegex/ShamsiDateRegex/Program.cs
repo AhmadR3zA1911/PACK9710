83f148a48e2f6af26367468d28b95ae8ae1532ec
AhmadR3zA1911 <46918547+AhmadR3zA1911@users.noreply.github.com>
2019-03-10 00:06:26 +0330

    Shamsi Date Regex
    a simple regex for detect shamsi date (Solar Hijri calendar).
    it is work for Gregorian calendar.

---
+++ MTA1-Traning/Tamrin[10]-ShamsiDateRegex/ShamsiDateRegex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ShamsiDateRegex
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Regex r = new Regex(@"([12]\d{3}\/((0[1-6])\/(0[1-9]|[12]\d|3[01])|((0[7-9]|1[0-2])\/(0[1-9]|[12]\d|3[0]))))|([12]\d{3}-((0[1-6])-(0[1-9]|[12]\d|3[01])|((0[7-9]|1[0-2])-(0[1-9]|[12]\d|3[0]))))");
+
+            for (;;)
+            {
+                Console.Write("Date (1397/01/01 || 1397-12-30):");
+                var text = Console.ReadLine();
+                if (text=="exit")
+                    break;
+                var isMatch = r.IsMatch(text);
+                if (isMatch)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Match!");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Not Match!");
+                }
+                
+                Console.ResetColor();
+            }
+        }
+    }
+}
