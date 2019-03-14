50f020f3e0d692423200d3f62057701ffd30bbfc
AhmadR3zA1911 <46918547+AhmadR3zA1911@users.noreply.github.com>
2019-03-15 02:34:30 +0330

    CopyFileToRemoveDrive
    copy new file from special folder to all remove drive v1

---
+++ MTA1-Traning/Tamrin[11]-CopyFile/CopyFileToRemoveDrive/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyFileToRemoveDrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Select Option number:");
+            MenuList();
+            for (;;)
+            {
+                
+
+                Console.ReadLine();
+            }
+
+        }
+
+
+
+        /// <summary>
+        /// Main Menu
+        /// </summary>
+        static void MenuList()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n\n_______________--------------------_______________");
+            Console.WriteLine("1.Copy This Session.");
+            Console.WriteLine("2.Select Source Folder.");
+            Console.WriteLine("3.Add New Book.");
+            Console.WriteLine("/--------------------_______________--------------------");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Enter Your Choice:");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+    }
+}
