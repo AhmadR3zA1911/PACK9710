ed5011f941a4368d1cfd87c2ad407b30e44c0601
AhmadR3zA1911 <46918547+AhmadR3zA1911@users.noreply.github.com>
2019-04-05 17:03:23 +0430

    Create Dynamic Button for Simple Calendar
    this simple project create dynamic button for days of month and after user click each button  , message box show Shamsi and Gregorian calendar to user.

---
+++ MTA1-Traning/Tamrin[13]-DynamicButtonCalender/Button-Calender/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Button_Calender
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbYear = new System.Windows.Forms.ComboBox();
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbMonth = new System.Windows.Forms.ComboBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbYear
+            // 
+            this.cmbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbYear.FormattingEnabled = true;
+            this.cmbYear.Location = new System.Drawing.Point(61, 25);
+            this.cmbYear.Name = "cmbYear";
+            this.cmbYear.Size = new System.Drawing.Size(78, 21);
+            this.cmbYear.TabIndex = 0;
+            // 
+            // lblMonth
+            // 
+            this.lblMonth.AutoSize = true;
+            this.lblMonth.Location = new System.Drawing.Point(21, 28);
+            this.lblMonth.Name = "lblMonth";
+            this.lblMonth.Size = new System.Drawing.Size(32, 13);
+            this.lblMonth.TabIndex = 1;
+            this.lblMonth.Text = "Year:";
+            this.lblMonth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Month:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cmbMonth
+            // 
+            this.cmbMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMonth.FormattingEnabled = true;
+            this.cmbMonth.Location = new System.Drawing.Point(206, 23);
+            this.cmbMonth.Name = "cmbMonth";
+            this.cmbMonth.Size = new System.Drawing.Size(78, 21);
+            this.cmbMonth.TabIndex = 0;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(326, 23);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(120, 23);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(61, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(411, 2);
+            this.label2.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 253);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMonth);
+            this.Controls.Add(this.cmbMonth);
+            this.Controls.Add(this.cmbYear);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calender";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbYear;
+        private System.Windows.Forms.Label lblMonth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbMonth;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MTA1-Traning/Tamrin[13]-DynamicButtonCalender/Button-Calender/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Button_Calender
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        
+        // global variable
+        List<Button> Buttonz = new List<Button>();
+        string year = "";
+        string month = "";
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            AddRangeNumbeCMByear(); // load year cmb
+            AddRangeNumbeCMBMonth(); //  Load Month
+            
+        }
+        
+
+        /// <summary>
+        /// Add Year To CmbYear From 1300 unti now
+        /// </summary>
+        private void AddRangeNumbeCMByear()
+        {
+            PersianCalendar p = new PersianCalendar();
+
+            for (int i = 1300; i <= p.GetYear(DateTime.Now); i++)
+            {
+                cmbYear.Items.Add(i);
+            }
+        }
+
+        /// <summary>
+        /// Add Month to Month 
+        /// </summary>
+        private void AddRangeNumbeCMBMonth()
+        {
+
+
+            for (int i = 01; i <= 12; i++)
+            {
+                cmbMonth.Items.Add(i);
+            }
+        }
+
+        /// <summary>
+        /// Get Day of Month (31 Or 30)
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        private int CheckNumberOfDayInMonth(string y, string m)
+        {
+            PersianCalendar p = new PersianCalendar();
+
+
+            int day = p.GetDaysInMonth(Convert.ToInt32(y), Convert.ToInt32(m));
+            return day;
+        }
+
+        /// <summary>
+        /// create dynamically button and Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+           
+            if (Buttonz.Count>0) // if form has buttons remove all of them : ) 
+            {
+                foreach (var b in Buttonz)
+                {
+                    this.Controls.Remove(b);
+                }
+                Buttonz.Clear();
+            }
+
+            
+            if (cmbMonth.SelectedItem != null && cmbYear.SelectedItem != null) // user selected cmb
+            {
+                year = cmbYear.SelectedItem.ToString(); //global 
+                month = cmbMonth.SelectedItem.ToString(); // global 
+
+                int d = CheckNumberOfDayInMonth(year, month); // get day
+                int top = 90;
+                int left = 50;
+                for (int i = 1; i <= d; i++)
+                {
+                    if (left>=433) // Each row must have 9 button :D 
+                    {
+                        top += 30; 
+                        left = 50;
+                    }
+
+                    Button b = new Button();
+                    this.Controls.Add(b);
+                    //b.Location = new Point(62, i + 23);
+                    b.Top = top;
+                    b.Left = left;
+                    b.Name = "btn" + i.ToString();
+                    b.Text = i.ToString();
+                    b.Size = new System.Drawing.Size(39, 23);
+                    b.Click += new EventHandler(this.Button_Click); // show date 
+                    Buttonz.Add(b);
+                    left = left + 46;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// envent for created button for show Shamsi and Gregorian
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click (object sender , EventArgs e)
+        {
+            PersianCalendar p = new PersianCalendar();
+            Button btn = sender as Button;
+            string g = p.ToDateTime(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(btn.Text),0,0,0,0).ToShortDateString();
+            string s = year + "/" + month + "/" + btn.Text;
+            MessageBox.Show("Shamsi: "+s+"\n\n"+ "Gregorian: " + g, "Shamsi And Gregorian");
+        }
+    }
+}
