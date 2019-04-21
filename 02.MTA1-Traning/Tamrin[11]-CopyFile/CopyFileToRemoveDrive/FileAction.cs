790d04adbd9047493c8b322f9e1acab3438ca8b7
AhmadR3zA1911 <46918547+AhmadR3zA1911@users.noreply.github.com>
2019-04-22 02:25:01 +0430

    change folder name

---
+++ 02.MTA1-Traning/Tamrin[11]-CopyFile/CopyFileToRemoveDrive/FileAction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Copy_File
+{
+    class FileAction
+    {
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// 
+        public FileAction()
+        {
+
+        }
+        /// <summary>
+        /// Set FilePath Variable
+        /// </summary>
+        /// <param name="_path"></param>
+        public FileAction(string _path)
+        {
+            this.FilePath = _path;
+        }
+
+        public string FindFilePath(string _path)
+        {
+            if (FilePath!=null)
+            {
+                return FilePath;
+            }
+
+            else
+
+            {
+                return FilePath;
+            }
+        }
+    }
+
+}
